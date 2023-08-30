a16257e60bd8aef6ba18b2f8942d119f79c752a5
Swetha Srikari Maganti <mss@Swethas-MacBook-Air.local>
2023-08-30 14:11:32 -0400

    Add learnable sinusoidal positional encoder
    Signed-off-by: Swetha Srikari Maganti <mss@Swethas-MacBook-Air.local>

---
+++ models/positional_encoding/LSPE.py
@@ -0,0 +1,40 @@
+import math
+import torch
+from torch import nn, Tensor
+
+class LearnableSPE(nn.Module):
+
+	def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 512):
+		super().__init__()
+		self.dropout = nn.Dropout(p=dropout)
+
+		position = torch.arange(max_len).unsqueeze(1)
+		div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+		pe = torch.zeros(max_len, 1, d_model)
+		pe[:, 0, 0::2] = torch.sin(position * div_term)
+		pe[:, 0, 1::2] = torch.cos(position * div_term)
+		self.register_buffer('pe', pe)
+
+	def forward(self, x: Tensor) -> Tensor:
+		"""
+		Arguments:
+		    x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+		"""
+		x = x + self.pe[:x.size(0)]
+		return self.dropout(x)
+
+class PositionwiseFeedForward(nn.Module):
+
+	def __init__(self, d_model, hidden, drop_prob=0.1):
+		super(PositionwiseFeedForward, self).__init__()
+		self.linear1 = nn.Linear(d_model, hidden)
+		self.linear2 = nn.Linear(hidden, d_model)
+		self.sigmoid = nn.Sigmoid()
+		self.dropout = nn.Dropout(p=drop_prob)
+
+	def forward(self, x):
+		x = self.linear1(x)
+		x = self.sigmoid(x)
+		x = self.dropout(x)
+		x = self.linear2(x)
+		return x

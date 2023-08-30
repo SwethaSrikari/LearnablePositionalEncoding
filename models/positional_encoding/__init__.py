0afd6fb0d1489f7aa4422e709feb42705c8ce751
Swetha Srikari Maganti <mss@Swethas-MacBook-Air.local>
2023-08-30 14:11:50 -0400

    Add init file
    Signed-off-by: Swetha Srikari Maganti <mss@Swethas-MacBook-Air.local>

---
+++ models/positional_encoding/__init__.py
@@ -0,0 +1,5 @@
+"""
+@LPSE code by: Swetha
+@Code reference: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+@Learnable Sinusoidal Positional encoding (LPSE): https://aclanthology.org/2022.findings-aacl.42.pdf
+"""
